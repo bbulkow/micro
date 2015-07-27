0898727172d33fab13ac49586b7b424ce2ea357a
Brian Bulkowski <brian@bulkowski.org>
2015-07-27 17:20:36 +0530

    Added PHP string catenation test

--- micro.php
+++ micro.php
@@ -1,17 +1,18 @@
 <?php
 
+ini_set('memory_limit','1G');
 
-function t1() {
+function f1() {
 	$start = microtime(true);
 	$x = 10;
 	for ($i = 0; $i < 100000; ++$i) {
 		$x = ( 2 * $x ) + $x + 1;
 	}
 #	print "t1 answer %d time %f" % (x, float(time.clock()) - start) 
-	printf( "t1 result %d time %f seconds\n" , $x, microtime(true) - $start );
+	printf( "f1 result %d time %f seconds\n" , $x, microtime(true) - $start );
 }
 
-function t1a() {
+function f1a() {
 	$start = microtime(true);
 	$x = 10;
 	$i = 0;
@@ -20,11 +21,44 @@ function t1a() {
 		++$i;
 	} while ($i < 100000);
 #	print "t1 answer %d time %f" % (x, float(time.clock()) - start) 
-	printf( "t1a result %d time %f seconds\n" , $x, microtime(true) - $start );
+	printf( "f1a (different loop) result %d time %f seconds\n" , $x, microtime(true) - $start );
+}
+
+function f2() {
+	$t0 = microtime(true);
+
+	$nStrings = 1000;
+	$nCats = 100000;
+
+	$a1 = array();
+	$c = ord( "a" );
+	$a1[0] = (string) chr($c);
+
+	for ($x = 1; $x < $nStrings; $x++) {
+		$a1[ $x ] = $a1[ $x - 1] . chr($c);
+	}
+
+	$t1 = microtime(true);
+
+	$a2 = array();
+	for ($x = 0; $x < $nCats; $x++) {
+		$a2[ $x ] = $a1[ $x % count($a1) ] . 
+					$a1[ ( $x + 7 ) % count($a1) ] . 
+					$a1[ ( $x + 17 ) % count($a1) ] . 
+					$a1[ ( $x + 27 ) % count($a1) ] . 
+					$a1[ ( $x + 37 ) % count($a1) ] . 
+					$a1[ ( $x + 47 ) % count($a1) ] .
+					$a1[ ( $x + 57 ) % count($a1) ] ;
+	}
+
+	$t2 = microtime(true);
+
+	printf( "f2 string create %f sec cat with dot %f\n" , $t1 - $t0, $t2 - $t1 );
+
 }
 
 
-function t5() {
+function f5() {
   $start = microtime(true);
   for ($i = 0; $i < 1000000; ++$i) {
      $a = array();
@@ -36,8 +70,9 @@ function t5() {
 }
 
 print "quick performance test\n";
-t1();
-t1a();
-t5();
+f1();
+f1a();
+f2();
+f5();
 print "end quick performance test\n";
 ?>
